b5c80464f8a041669b9778bfff8020436c15e8b3
John Flatness <john@zerocrates.org>
2021-02-11 17:14:09 -0500

    Allow CSV Import of rights statement values
    Bump version to 1.2.0

--- config/module.config.php
+++ config/module.config.php
@@ -5,4 +5,12 @@ return [
             'rights_statement' => 'RightsStatements\DataType\RightsStatement',
         ],
     ],
+    'csv_import' => [
+        'data_types' => [
+            'rights_statement' => [
+                'label' => 'Rights Statement',
+                'adapter' => 'uri',
+            ],
+        ],
+    ],
 ];
